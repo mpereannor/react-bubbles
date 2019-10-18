6c50dd263a482c4551c22be06099be62169596fd
chudnofskyATD <mpereannor@gmail.com>
2019-10-18 07:18:19 +0000

    stage 2 bubblespage.js fetch color data for bubbles v1

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -1,11 +1,26 @@
 import React, { useState, useEffect } from "react";
+import axiosWithAuth from '../axios';
 import axios from "axios";
 
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
+import { axiosWithAuth } from "../axios";
 
+const colorApi = 'https://localhost:5000//api/colors';
 const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
+
+  useEffect(() => {
+    axiosWithAuth()
+    .get(colorApi)
+    .then(response => {
+      setColorList(response.data)
+    })
+    .catch(error => {
+      alert(error.response.data.message);
+    })
+
+  }, [])
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
 
